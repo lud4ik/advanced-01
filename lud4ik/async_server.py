e8044e37ce464e91c05ebb9031af036c3608e431
Luda Lukashevskaya <lukashe@ukr.net>
2012-12-21 21:20:54 +0200

    async changes

--- lud4ik/async_server.py
+++ lud4ik/async_server.py
@@ -1,19 +1,11 @@
-import os
-import os.path
 import socket
-import signal
-import logging
-import threading
 from functools import partial
-from operator import attrgetter
-from collections import namedtuple
 from select import epoll, EPOLLIN, EPOLLOUT, EPOLLET, EPOLLHUP, EPOLLERR
 
-from work.protocol import Feeder
 from work.models import cmd
+from work.protocol import Feeder
 from work.cmdargs import get_cmd_args
-from work.exceptions import ServerFinishException
-from work.utils import get_random_hash, handle_timeout, configure_logging
+from work.utils import get_random_hash
 
 
 READ_ONLY = EPOLLIN | EPOLLHUP | EPOLLERR
@@ -26,7 +18,8 @@ class ClientHandler:
     CHUNK_SIZE = 1024
     commands = [cmd.CONNECT, cmd.PING, cmd.PINGD, cmd.QUIT, cmd.FINISH]
 
-    def __init__(self, conn, addr):
+    def __init__(self, server, conn, addr):
+        self.server = server
         self.conn = conn
         self.addr = addr
         self.in_buffer = bytes()
@@ -38,10 +31,10 @@ class ClientHandler:
         if event in ERRORS:
             print('error')
         elif event == EPOLLIN:
-            self.handle_read()
+            print('EPOLLIN')
         elif event == EPOLLOUT:
-            if self.out_buffer:
-                self.handle_write()
+            print('EPOLLOUT')
+            self.handle_read()
 
     def handle_write(self):
         sent = self.conn.send(self.out_buffer)
@@ -58,6 +51,29 @@ class ClientHandler:
     def connect(self, packet):
         reply = packet.reply(self.session)
         self.out_buffer += reply
+        self.handle_write()
+
+    def ping(self, packet):
+        reply = packet.reply()
+        self.out_buffer += reply
+        self.handle_write()
+
+    def pingd(self, packet):
+        reply = packet.reply()
+        self.out_buffer += reply
+        self.handle_write()
+
+    def quit(self, packet):
+        reply = packet.reply(self.session)
+        self.out_buffer += reply
+        self.handle_write()
+        self.server.quit_client(self.conn)
+
+    def finish(self, packet):
+        reply = packet.reply()
+        self.out_buffer += reply
+        self.handle_write()
+        self.server.shutdown()
 
 
 class AsyncCommandServer:
@@ -65,6 +81,7 @@ class AsyncCommandServer:
     MAX_CONN = 5
 
     def __init__(self, poller, host, port):
+        self.clients = []
         self.poller = poller
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
@@ -76,26 +93,47 @@ class AsyncCommandServer:
         if event in ERRORS:
             print('error')
         conn, addr = self.socket.accept()
+        self.clients.append(conn)
         self.poller.register(conn, EDGE_MASK)
-        handle_client = ClientHandler(conn, addr)
+        handle_client = ClientHandler(self, conn, addr)
         handlers[conn.fileno()] = handle_client
 
+    def quit_client(self, conn):
+        self.clients.remove(conn)
+        self.poller.unregister(conn.fileno())
+        conn.close()
 
-def main():
-    args = get_cmd_args()
+    def shutdown(self):
+        self.poller.unregister(self.socket.fileno())
+        self.socket.close()
+        for conn in self.clients:
+            self.poller.unregister(conn.fileno())
+            conn.close()
+        self.poller.close()
+        raise SystemExit()
 
-    poller = epoll()
-    async_server = AsyncCommandServer(poller, args.host, args.port)
-    handlers = {}
-    handlers[async_server.socket.fileno()] = partial(async_server.handle_accept,
-                                                     handlers)
-    poller.register(async_server.socket, READ_ONLY)
 
-    while True:
-        for (fd, event) in poller.poll():
-            handlers[fd](fd, event)
+class Reactor:
+
+    INTERVAL = 0.1
+
+    def __init__(self):
+        self.poller = epoll()
+        self.handlers = {}
+
+    def register_server(self, sock, handler, mask=READ_ONLY):
+        self.handlers[sock.fileno()] = partial(handler, self.handlers)
+        self.poller.register(sock, mask)
+
+    def run(self):
+        while True:
+            for (fd, event) in self.poller.poll(self.INTERVAL):
+                self.handlers[fd](fd, event)
 
 
 if __name__ == '__main__':
-    configure_logging('Server')
-    main()
+    args = get_cmd_args()
+    reactor = Reactor()
+    async_server = AsyncCommandServer(reactor.poller, args.host, args.port)
+    reactor.register_server(async_server.socket, async_server.handle_accept)
+    reactor.run()
